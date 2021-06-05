b446dd0070b72775ce8ef1bfe6c5f2f2779b65e2
teppchan <teppchan+github@github.com>
2021-06-05 21:56:41 +0900

    Added day09d

---
+++ day09d/kernel/console.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "graphics.hpp"
+
+class Console
+{
+public:
+    static const int kRows = 25;
+    static const int kColumns = 80;
+
+    Console(const PixelColor &fg_color, const PixelColor &bg_color);
+    void PutString(const char *s);
+    void SetWriter(PixelWriter* writer);
+
+private:
+    void Newline();
+    void Refresh();
+
+    PixelWriter* writer_;
+    const PixelColor fg_color_;
+    const PixelColor bg_color_;
+    char buffer_[kRows][kColumns + 1];
+    int cursor_row_;
+    int cursor_column_;
+};
---
+++ day09d/kernel/layer.hpp
@@ -0,0 +1,53 @@
+#pragma once
+
+#include <memory>
+#include <map>
+#include <vector>
+
+#include "graphics.hpp"
+#include "window.hpp"
+
+class Layer
+{
+public:
+    Layer(unsigned int id = 0);
+    unsigned int ID() const;
+
+    Layer &SetWindow(const std::shared_ptr<Window> &window);
+    std::shared_ptr<Window> GetWindow() const;
+
+    Layer &Move(Vector2D<int> pos);
+    Layer &MoveRelative(Vector2D<int> pos_diff);
+
+    void DrawTo(FrameBuffer& screen) const;
+
+private:
+    unsigned int id_;
+    Vector2D<int> pos_;
+    std::shared_ptr<Window> window_;
+};
+
+class LayerManager
+{
+public:
+    void SetWriter(FrameBuffer* screen);
+
+    Layer &NewLayer();
+    void Draw() const;
+
+    void Move(unsigned int id, Vector2D<int> new_position);
+    void MoveRelative(unsigned int id, Vector2D<int> pos_diff);
+
+    void UpDown(unsigned int id, int new_height);
+    void Hide(unsigned int id);
+
+private:
+    FrameBuffer* screen_{nullptr};
+    std::vector<std::unique_ptr<Layer>> layers_{};
+    std::vector<Layer *> layer_stack_{};
+    unsigned int latest_id_{9};
+
+    Layer *FindLayer(unsigned int id);
+};
+
+extern LayerManager *layer_manager;
