f209b6d0ee01dc14120f197fabe1157886bc4cc9
teppchan <teppchan+github@github.com>
2021-06-10 21:11:50 +0900

    Added day10g

---
+++ day10g/bat.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+pushd ../../edk2
+rm MikanLoaderPkg  # remove symbolic link
+ln -s ../MikanOS/day10g/MikanLoaderPkg
+source edksetup.sh
+#\rm -r Build
+build
+
+popd
+
+../../osbook/devenv/run_qemu.sh ../../edk2/Build/MikanLoaderX64/DEBUG_CLANG38/X64/Loader.efi kernel/kernel.elf
