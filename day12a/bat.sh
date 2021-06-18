62493dc37b46961fb17ca3af392ddd823438c7c2
teppchan <teppchan+github@github.com>
2021-06-18 21:57:36 +0900

    Added day12a

---
+++ day12a/bat.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+pushd ../../edk2
+rm MikanLoaderPkg  # remove symbolic link
+ln -s ../MikanOS/day12a/MikanLoaderPkg
+source edksetup.sh
+#\rm -r Build
+build
+
+popd
+
+../../osbook/devenv/run_qemu.sh ../../edk2/Build/MikanLoaderX64/DEBUG_CLANG38/X64/Loader.efi kernel/kernel.elf
+#../../osbook/devenv/run_qemu.sh ../../edk2/Build/MikanLoaderX64/DEBUG_CLANG38/X64/Loader.efi ../mikanos/kernel/kernel.elf
