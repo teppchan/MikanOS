213b18a1ca60c016bb041c243a363cbaf8631b63
teppchan <teppchan+github@github.com>
2021-08-12 20:01:37 +0900

    Added day23a

---
+++ day23a/kernel/app_event.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+struct AppEvent {
+    enum Type {
+        kQuit,
+        kMouseMove,
+    } type;
+
+    union {
+        struct {
+            int x, y;
+            int dx, dy;
+            uint8_t buttons;
+        } mouse_move;
+    } arg;
+};
+
+#ifdef __cplusplus
+}
+#endif
