72061597f75fa8100d34c2c711ce6f1bfa0c2b43
teppchan <teppchan+github@github.com>
2021-09-05 22:00:11 +0900

    Added day28c

---
+++ day28c/kernel/app_event.hpp
@@ -0,0 +1,53 @@
+#pragma once
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+    struct AppEvent
+    {
+        enum Type
+        {
+            kQuit,
+            kMouseMove,
+            kMouseButton,
+            kTimerTimeout,
+            kKeyPush,
+        } type;
+
+        union
+        {
+            struct
+            {
+                int x, y;
+                int dx, dy;
+                uint8_t buttons;
+            } mouse_move;
+
+            struct
+            {
+                int x, y;
+                int press;
+                int button;
+            } mouse_button;
+
+            struct
+            {
+                unsigned long timeout;
+                int value;
+            } timer;
+
+            struct
+            {
+                uint8_t modifier;
+                uint8_t keycode;
+                char ascii;
+                int press;
+            } keypush;
+        } arg;
+    };
+
+#ifdef __cplusplus
+}
+#endif
