d51b2c157ea296e48d7b285e19ba0f62c1ee07fe
teppchan <teppchan+github@github.com>
2021-09-11 20:34:06 +0900

    Added day30a

---
+++ day30a/apps/grep/grep.cpp
@@ -0,0 +1,32 @@
+#include <cstdio>
+#include <cstdlib>
+#include <regex>
+
+extern "C" void main(int argc, char **argv)
+{
+    if (argc < 2)
+    {
+        printf("Usage: %s <pattern> [<file>]\n", argv[0]);
+        exit(1);
+    }
+
+    std::regex pattern{argv[1]};
+
+    FILE *fp = stdin;
+    if (argc >= 3 && (fp = fopen(argv[2], "r")) == nullptr)
+    {
+        fprintf(stderr, "failed to open: %s", argv[2]);
+        exit(1);
+    }
+
+    char line[256];
+    while (fgets(line, sizeof(line), fp))
+    {
+        std::cmatch m;
+        if (std::regex_search(line, m, pattern))
+        {
+            printf("%s", line);
+        }
+    }
+    exit(0);
+}
---
+++ day30a/bat.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+export APPS_DIR=apps
+export RESOURCE_DIR=resource
+
+(cd kernel; make -j8)
+
+for MK in $(ls apps/*/Makefile); do
+    APP_DIR=$(dirname $MK)
+    APP=$(basename $APP_DIR)
+    make -C $APP_DIR $APP
+done
+
+pushd ../../edk2
+rm MikanLoaderPkg  # remove symbolic link
+ln -s ../MikanOS/day30a/MikanLoaderPkg
+source edksetup.sh
+#\rm -r Build
+build
+
+popd
+
+#export QEMU_OPTS="-gdb tcp::12345"
+#../../osbook/devenv/run_qemu.sh ../../edk2/Build/MikanLoaderX64/DEBUG_CLANG38/X64/Loader.efi kernel/kernel.elf apps/onlyhlt/onlyhlt
+MIKANOS_DIR=$PWD ../../osbook/devenv/run_mikanos.sh
